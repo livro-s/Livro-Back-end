dfb4dcff98dce66cc280dadd5ed23598c58ab25e
yangseungbin <tanja1206@naver.com>
2020-11-23 22:05:33 +0900

    [ADD] user entity

---
+++ src/models/user.ts
@@ -0,0 +1,52 @@
+import { sequelize } from "../config/config";
+import Sequelize, { Model } from "sequelize";
+import { Notice } from "./notice";
+import { Loan } from "./loan";
+
+export class User extends Model {
+  uuid: string;
+  userId: string;
+  password: string;
+  name: string;
+  school: string;
+  studentNo: string;
+}
+
+User.init(
+  {
+    uuid: {
+      type: Sequelize.STRING(50),
+      primaryKey: true,
+    },
+    userId: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+      unique: true,
+      field: "user_id",
+    },
+    password: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    },
+    school: {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    },
+    studentNo: {
+      type: Sequelize.CHAR(4),
+      allowNull: false,
+      field: "student_no",
+    },
+  },
+  { sequelize, modelName: "user" }
+);
+
+User.hasMany(Notice, { foreignKey: "userUuid", sourceKey: "uuid" });
+Notice.belongsTo(User, { foreignKey: "userUuid" });
+
+User.hasMany(Loan, { foreignKey: "userUuid", sourceKey: "uuid" });
+Loan.belongsTo(User, { foreignKey: "userUuid" });
