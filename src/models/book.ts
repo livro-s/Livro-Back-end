2b7cb641bf8b0896e5669f0fd045f2d1add4d454
yangseungbin <tanja1206@naver.com>
2020-11-23 22:05:18 +0900

    [ADD] book entity

---
+++ src/models/book.ts
@@ -0,0 +1,48 @@
+import { sequelize } from "../config/config";
+import Sequelize, { Model } from "sequelize";
+import { Loan } from "./loan";
+
+export class Book extends Model {
+  id: string;
+  title: string;
+  author: string;
+  publisher: string;
+  category: string;
+  location: string;
+}
+
+Book.init(
+  {
+    id: {
+      type: Sequelize.STRING(10),
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },
+    author: {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    },
+    publisher: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },
+    category: {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    },
+    location: {
+      type: Sequelize.STRING(40),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "book",
+  }
+);
+
+Book.hasMany(Loan, { foreignKey: "bookId", sourceKey: "id" });
+Loan.belongsTo(Book, { foreignKey: "bookId" });
