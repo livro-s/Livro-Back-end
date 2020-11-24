ad1d219f6035ad73de02fc9ec46afa01bc88fc17
yangseungbin <tanja1206@naver.com>
2020-11-24 11:02:24 +0900

    [ADD] write notice

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import * as AdminController from "../controllers/admin";
 import { tryCatchMiddleware } from "../middlewares/tryCatch";
+import { authMiddleware } from "../middlewares/auth";
 
 const router: Router = Router();
 
 router.post("/auth", tryCatchMiddleware.Error(AdminController.adminAuth));
+router.post(
+  "/notice",
+  authMiddleware,
+  tryCatchMiddleware.Error(AdminController.writeNotice)
+);
 
 export default router;
