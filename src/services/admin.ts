f6485b4b87ce4c00933ac383582b4859152692a9
yangseungbin <tanja1206@naver.com>
2020-11-24 10:50:11 +0900

    [ADD] write notice

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -4,6 +4,7 @@ import { User } from "../models/user";
 import { Notice } from "../models/notice";
 import { mkAccess } from "../utils/mkToken";
 import { hashPassword } from "../utils/hash";
+import { mkId } from "../utils/uuid";
 
 export const adminAuthService = async (
   adminAuthDTO: IAdminAuthDTO,
@@ -11,7 +12,7 @@ export const adminAuthService = async (
 ) => {
   const { userId, password }: IAdminAuthDTO = adminAuthDTO;
   const user: User = await findOneUser(userId);
-  if (passwordCompare(password, user.password))
+  if (!passwordCompare(password, user.password))
     throw new HttpError(404, "User Not Found");
   await isAdmin(user.admin);
   return await mkAccess(user.uuid, user.admin, secret);
@@ -35,11 +36,16 @@ const passwordCompare = async (password: string, hashedPassword: string) => {
 
 export const writeNoticeService = async (
   writeNoticeDTO: IWriteNoticeDTO,
-  coverImg: string | null,
   uuid: string,
   admin: boolean
 ) => {
+  const noticeId = "notice-" + (await mkId());
   const { title, content }: IWriteNoticeDTO = writeNoticeDTO;
   await isAdmin(admin);
-  await Notice.create({ title, content, coverImg, userUuid: uuid });
+  await Notice.create({
+    uuid: noticeId,
+    title,
+    content,
+    userUuid: uuid,
+  });
 };
