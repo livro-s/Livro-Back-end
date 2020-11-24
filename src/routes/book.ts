b6922a85f9e0c73994cd44702e690748e49ba4fd
hmoon630 <s19017@gsm.hs.kr>
2020-11-24 15:06:07 +0900

    [ADD] book loan routes

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { tryCatchMiddleware } from "../middlewares/tryCatch";
-import { searchBook } from "../controllers/book";
+import { borrowBook, searchBook } from "../controllers/book";
+import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
 router.get("/", tryCatchMiddleware.Error(searchBook));
+
+router.post("/loan", authMiddleware, tryCatchMiddleware.Error(borrowBook));
 
 export default router;
