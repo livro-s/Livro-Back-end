6468dc00e2820aa520ead579610b65d8e7147974
yangseungbin <tanja1206@naver.com>
2020-11-24 09:55:46 +0900

    [ADD] admin auth service

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -1,20 +1,45 @@
-import { HttpError } from "exception/exception";
-import { IAdminAuthDTO } from "../interfaces/admin";
+import { HttpError } from "../exception/exception";
+import { IAdminAuthDTO, IWriteNoticeDTO } from "../interfaces/admin";
 import { User } from "../models/user";
+import { Notice } from "../models/notice";
+import { mkAccess } from "../utils/mkToken";
+import { hashPassword } from "../utils/hash";
 
-export const adminAuthService = async (adminAuthDTO: IAdminAuthDTO) => {
-  const user = await findOneUser(adminAuthDTO.userId);
-  if (!isAdmin(user)) throw new HttpError(409, "User Not Admin");
+export const adminAuthService = async (
+  adminAuthDTO: IAdminAuthDTO,
+  secret: string
+) => {
+  const { userId, password }: IAdminAuthDTO = adminAuthDTO;
+  const user: User = await findOneUser(userId);
+  if (passwordCompare(password, user.password))
+    throw new HttpError(404, "User Not Found");
+  await isAdmin(user.admin);
+  return await mkAccess(user.uuid, user.admin, secret);
 };
 
-const isAdmin = async (user: any): Promise<boolean> => {
-  return user.admin ? true : false;
-};
-
-const findOneUser = async (userId: string): Promise<object> => {
+const findOneUser = async (userId: string): Promise<User> => {
   try {
     return User.findOne({ where: { userId } });
   } catch (e) {
     throw new HttpError(404, "User Not Found");
   }
+};
+
+const isAdmin = async (admin: boolean) => {
+  if (!admin) throw new HttpError(409, "User Not Admin");
+};
+
+const passwordCompare = async (password: string, hashedPassword: string) => {
+  return (await hashPassword(password)) === hashedPassword;
+};
+
+export const writeNoticeService = async (
+  writeNoticeDTO: IWriteNoticeDTO,
+  coverImg: string | null,
+  uuid: string,
+  admin: boolean
+) => {
+  const { title, content }: IWriteNoticeDTO = writeNoticeDTO;
+  await isAdmin(admin);
+  await Notice.create({ title, content, coverImg, userUuid: uuid });
 };
